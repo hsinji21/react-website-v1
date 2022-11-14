325acc561bc10b57184376b28ee3c3ecd8f14935
hsinji21 <93311808+hsinji21@users.noreply.github.com>
2022-11-14 16:08:23 +0000

    Pending changes exported from your codespace

---
+++ build/precache-manifest.5dc94fc01932775cdde641dbf082a327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e616122f54986e4df95e1422c1901f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc36c4c277a1ff51b92",
+    "url": "/static/css/main.9ee91a79.chunk.css"
+  },
+  {
+    "revision": "7355874f27c05a80a7db",
+    "url": "/static/js/2.7ee65955.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7ee65955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc36c4c277a1ff51b92",
+    "url": "/static/js/main.eb673057.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
